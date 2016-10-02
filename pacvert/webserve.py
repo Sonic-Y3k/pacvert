c39bff3241bd997e49f886e5b3eb1a2defeaa81b
Sonic-Y3k <sonic.y3k@googlemail.com>
2016-10-02 21:57:00 +0200

    Webserver is somewhat running.

--- pacvert/webserve.py
+++ pacvert/webserve.py
@@ -1,8 +1,63 @@
 # This file is part of packvert.
 
+import os
+
 import pacvert
 import logger
 
 import cherrypy
+from cherrypy.lib.static import serve_file, serve_download
+from cherrypy._cperror import NotFound
 
+from mako.lookup import TemplateLookup
+from mako import exceptions
 
+def serve_template(templatename, **kwargs):
+    interface_dir = os.path.join(str(pacvert.PROG_DIR), 'data/interfaces/')
+    template_dir = os.path.join(str(interface_dir), pacvert.CONFIG.INTERFACE)
+
+    _hplookup = TemplateLookup(directories=[template_dir], default_filters=['unicode', 'h'])
+
+    server_name = "Pacvert"
+
+    try:
+        template = _hplookup.get_template(templatename)
+        return template.render(http_root=pacvert.HTTP_ROOT, server_name=server_name,
+                              **kwargs)
+    except:
+        return exceptions.html_error_template().render()
+
+class WebInterface(object):
+    """
+    """
+
+    def __init__(self):
+        self.interface_dir = os.path.join(str(pacvert.PROG_DIR), 'data/')
+
+    @cherrypy.expose
+    def index(self, **kwargs):
+        if pacvert.CONFIG.FIRST_RUN_COMPLETE:
+            raise cherrypy.HTTPRedirect(pacvert.HTTP_ROOT + "home")
+        else:
+            raise cherrypy.HTTPRedirect(pacvert.HTTP_ROOT + "welcome")
+
+    ##### Welcome #####
+    @cherrypy.expose
+    def welcome(self, **kwargs):
+        config = {
+        }
+
+        # The setup wizard just refreshes the page on submit so we must redirect to home if config set.
+        if pacvert.CONFIG.FIRST_RUN_COMPLETE:
+            pacvert.initialize_scheduler()
+            raise cherrypy.HTTPRedirect(pacvert.HTTP_ROOT + "home")
+        else:
+            return serve_template(templatename="welcome.html", title="Welcome", config=config)
+
+    ##### home #####
+    @cherrypy.expose
+    def home(self, **kwargs):
+        config = {
+
+        }
+        return serve_template(templatename="home.html", title="Home", config=config)
--- pacvert/webstart.py
+++ pacvert/webstart.py
@@ -1,11 +1,102 @@
 # This file is part of pacvert.
 
+import os
+
 import pacvert
 import cherrypy
 import logger
+
+from pacvert.helpers import create_https_certificates
+from pacvert.webserve import WebInterface
 
 def initialize(options):
     """
     """
 
-    logger.info("Web")
+    # HTTPS stuff stolen from sickbeard
+    enable_https = options['enable_https']
+    https_cert = options['https_cert']
+    https_key = options['https_key']
+
+    if enable_https:
+        # If either the HTTPS certificate or key do not exist, try to make self-signed ones.
+        if pacvert.CONFIG.HTTPS_CREATE_CERT and \
+            (not (https_cert and os.path.exists(https_cert)) or not (https_key and os.path.exists(https_key))):
+            if not create_https_certificates(https_cert, https_key):
+                logger.warn(u"Pacvert WebStart :: Unable to create certificate and key. Disabling HTTPS")
+                enable_https = False
+
+        if not (os.path.exists(https_cert) and os.path.exists(https_key)):
+            logger.warn(u"Pacvert WebStart :: Disabled HTTPS because of missing certificate and key.")
+            enable_https = False
+
+    options_dict = {
+        'server.socket_port': options['http_port'],
+        'server.socket_host': options['http_host'],
+        'environment': options['http_environment'],
+        'server.thread_pool': 10,
+        'tools.encode.on': True,
+        'tools.encode.encoding': 'utf-8',
+        'tools.decode.on': True
+    }
+
+    if enable_https:
+        options_dict['server.ssl_certificate'] = https_cert
+        options_dict['server.ssl_private_key'] = https_key
+        protocol = "https"
+    else:
+        protocol = "http"
+
+    if options['http_password']:
+        logger.info(u"Pacvert WebStart :: Web server authentication is enabled, username is '%s'", options['http_username'])
+        if options['http_basic_auth']:
+            auth_enabled = session_enabled = False
+            basic_auth_enabled = True
+        else:
+            options_dict['tools.sessions.on'] = auth_enabled = session_enabled = True
+            basic_auth_enabled = False
+            cherrypy.tools.auth = cherrypy.Tool('before_handler', webauth.check_auth)
+    else:
+        auth_enabled = session_enabled = basic_auth_enabled = False
+
+    if not options['http_root'] or options['http_root'] == '/':
+        pacvert.HTTP_ROOT = options['http_root'] = '/'
+    else:
+        pacvert.HTTP_ROOT = options['http_root'] = '/' + options['http_root'].strip('/') + '/'
+
+    cherrypy.config.update(options_dict)
+
+    #some parts missing
+    conf = {
+        '/': {
+            'tools.staticdir.root': os.path.join(pacvert.PROG_DIR, 'data'),
+            'tools.proxy.on': options['http_proxy'],  # pay attention to X-Forwarded-Proto header
+            'tools.gzip.on': True,
+            'tools.gzip.mime_types': ['text/html', 'text/plain', 'text/css',
+                                      'text/javascript', 'application/json',
+                                      'application/javascript'],
+            'tools.auth.on': auth_enabled,
+            'tools.sessions.on': session_enabled,
+            'tools.sessions.timeout': 30 * 24 * 60,  # 30 days
+            'tools.auth_basic.on': basic_auth_enabled,
+            'tools.auth_basic.realm': 'Pacvert web server',
+            'tools.auth_basic.checkpassword': cherrypy.lib.auth_basic.checkpassword_dict({
+                options['http_username']: options['http_password']})
+        }
+    }
+
+    # Prevent time-outs
+    cherrypy.engine.timeout_monitor.unsubscribe()
+    cherrypy.tree.mount(WebInterface(), options['http_root'], config=conf)
+
+    try:
+        logger.info(u"Pacvert WebStart :: Starting Pacvert web server on %s://%s:%d%s", protocol,
+                    options['http_host'], options['http_port'], options['http_root'])
+        cherrypy.process.servers.check_port(str(options['http_host']), options['http_port'])
+
+        cherrypy.server.start()
+    except IOError:
+        sys.stderr.write('Failed to start on port: %i. Is something else running?\n' % (options['http_port']))
+        sys.exit(1)
+
+    cherrypy.server.wait()
