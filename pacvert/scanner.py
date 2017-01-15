b5bd49911d99a03f4d1edf26055d83f975322173
Sonic-Y3k <sonic.y3k@googlemail.com>
2017-01-15 19:04:28 +0100

    apply filter a little earlier

--- pacvert/scanner.py
+++ pacvert/scanner.py
@@ -14,13 +14,13 @@ def scan():
         for root, directories, filenames in walk(pacvert.CONFIG.SCAN_DIRECTORIES_PATH):
             for filename in sorted(filenames):
                 full_path = path.join(root,filename)
-                if full_path not in pacvert.IGNORE_QUEUE:
+                if full_path not in pacvert.IGNORE_QUEUE and pacvert.CONFIG.SEARCH_FILE_FORMATS in full_path:
                     test_file = QueueElement(full_path)
                     
                     if test_file.file_validity() and test_file.file_configure():
                         pacvert.IGNORE_QUEUE.append(full_path)
                         pacvert.QUEUE.append('pending', test_file)
                     else:
-                        logger.error('can\'t add file \''+full_path+'\' to queue (probably to young).')
+                        logger.error('can\' add '+full_path+' to queue. Probably to young.')
     except Exception as e:
         logger.error('scanning directory \''+pacvert.CONFIG.SCAN_DIRECTORIES_PATH+'\' failed with: '+e.message)
